6b513a1a948207119dcc2e840adbe45ae1904959
Adam Imane <adamdeardon@hotmail.com>
2025-11-01 21:01:59 +0000

    Added an event listener to the sidebar so that it automatically closes when needed.

--- header1/header1.js
+++ header1/header1.js
@@ -1,7 +1,13 @@
 // Toggle sidebar animation
 function toggleSidebar() {
-    // Get the nav menu
     let navbar = document.getElementById('navigation-bar');
-    // Toggle the closed class
     navbar.classList.toggle('closed');
 }
+
+// Hide sidebar automatically when zooming out.
+window.addEventListener('resize', () => {
+    let navbar = document.getElementById('navigation-bar');
+    if (window.innerWidth > 1250 && !navbar.classList.contains('closed')) {
+        navbar.classList.add('closed');
+    }
+});
