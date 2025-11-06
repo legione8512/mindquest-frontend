e0e1eb4001f0a02747e8b3131c29217dbbe640ee
Adam Imane <adamdeardon@hotmail.com>
2025-11-06 19:44:48 +0000

    - Fixed scaling for the sidebar.

--- header1/header1.js
+++ header1/header1.js
@@ -1,13 +1,13 @@
 // Toggle sidebar animation
 function toggleSidebar() {
-    let sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("closed");
+    const wrapper = document.getElementById("sidebar-wrapper");
+    wrapper.classList.toggle("closed");
 }
 
 // Hide sidebar automatically when zooming out.
 window.addEventListener('resize', () => {
-    let sidebar = document.getElementById('sidebar');
-    if (window.innerWidth > 1250 && !sidebar.classList.contains('closed')) {
-        sidebar.classList.add('closed');
+    const wrapper = document.getElementById("sidebar-wrapper");
+    if (window.innerWidth > 1250 && !wrapper.classList.contains('closed')) {
+        wrapper.classList.add('closed');
     }
 });
