fc593fe101749c47209fd02131d45cb2e0311e24
Adam Imane <adamdeardon@hotmail.com>
2025-11-21 17:04:33 +0000

    Footer changes:
    - Adjusted footer spacing betwen content.
    
    - Still waiting for actual page content to begin resizing changes.

--- front-end/src/components/footer/footer.jsx
+++ front-end/src/components/footer/footer.jsx
@@ -7,7 +7,8 @@ export default function Footer() {
         <footer>
             {/* MindQuest Footer Logo */}
             <section>
-                <img src={logo} alt="MindQuest Logo" />
+                <Link to="/"><img src={logo} alt="MindQuest Logo" /></Link>
+
                 {/* Social media section */}
                 <section className="footer_follow">
                     <Link to="https://x.com/">
@@ -25,23 +26,23 @@ export default function Footer() {
                 </section>
             </section>
 
+            <section className="section_boxes">
+                {/* Contact Section for footer */}
+                <section className="footer_section">
+                    <h3>Contact Information</h3>
+                    <p><i className="fa-solid fa-envelope"></i>SupportTeam@Mindquest.co.uk</p>
+                    <p><i className="fa-solid fa-phone"></i>+44 7911036333</p>
+                    <p><i className="fa-sharp fa-solid fa-location-dot"></i>31 Kingston Lane, Uxbridge <br />&emsp;&emsp;Middlesex, UB8 3PH</p>
+                </section>
 
-
-            {/* Contact Section for footer */}
-            <section className="footer_section">
-                <h3>Contact Information</h3>
-                <p><i className="fa-solid fa-envelope"></i>SupportTeam@Mindquest.co.uk</p>
-                <p><i className="fa-solid fa-phone"></i>+44 7911036333</p>
-                <p><i className="fa-sharp fa-solid fa-location-dot"></i>31 Kingston Lane, Uxbridge <br />&emsp;&emsp;Middlesex, UB8 3PH</p>
-            </section>
-
-            {/* Privacy and Security section for footer*/}
-            <section className="footer_section">
-                <h3>Privacy and Security</h3>
-                <ul>
-                    <li><Link className="privacy_button" to="">Privacy Policy</Link></li>
-                    <li><Link className="privacy_button" to="">Terms and Conditions</Link></li>
-                </ul>
+                {/* Privacy and Security section for footer*/}
+                <section className="footer_section">
+                    <h3>Privacy and Security</h3>
+                    <ul>
+                        <li><Link className="privacy_button" to="">Privacy Policy</Link></li>
+                        <li><Link className="privacy_button" to="">Terms and Conditions</Link></li>
+                    </ul>
+                </section>
             </section>
         </footer>
     );
