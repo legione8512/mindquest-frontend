3340fc94e4005004bf971865795b896e40f5ad52
Adam Imane <adamdeardon@hotmail.com>
2025-11-21 16:51:33 +0000

    Navigation and Footer changes:
    - Changed header buttons (navigation + login) sizes to account for multiple zooms in and out.
    
    - Adjusted footer styling to be cleaner.
    
    - Added Privacy and security links to the footer.

--- front-end/src/components/footer/footer.jsx
+++ front-end/src/components/footer/footer.jsx
@@ -8,31 +8,40 @@ export default function Footer() {
             {/* MindQuest Footer Logo */}
             <section>
                 <img src={logo} alt="MindQuest Logo" />
+                {/* Social media section */}
+                <section className="footer_follow">
+                    <Link to="https://x.com/">
+                        <button className="footer-button"><i className="fa-brands fa-twitter"></i></button>
+                    </Link>
+                    <Link to="https://www.instagram.com/">
+                        <button className="footer-button"><i className="fa-brands fa-instagram"></i></button>
+                    </Link>
+                    <Link to="https://www.youtube.com/">
+                        <button className="footer-button"><i className="fa-brands fa-youtube"></i></button>
+                    </Link>
+                    <Link to="https://www.linkedin.com/">
+                        <button className="footer-button"><i className="fa-brands fa-linkedin"></i></button>
+                    </Link>
+                </section>
             </section>
 
-            {/* Social media section*/}
-            <section className="footer_follow">
-                <h3>Follow Us</h3>
-                <Link to="https://x.com/">
-                    <button className="footer-button"><i className="fa-brands fa-twitter"></i></button>
-                </Link>
-                <Link to="https://www.instagram.com/">
-                    <button className="footer-button"><i className="fa-brands fa-instagram"></i></button>
-                </Link>
-                <Link to="https://www.youtube.com/">
-                    <button className="footer-button"><i className="fa-brands fa-youtube"></i></button>
-                </Link>
-                <Link to="https://www.linkedin.com/">
-                    <button className="footer-button"><i className="fa-brands fa-linkedin"></i></button>
-                </Link>
-            </section>
 
-            {/* Contact Section for footer*/}
-            <section className="footer_contact">
+
+            {/* Contact Section for footer */}
+            <section className="footer_section">
                 <h3>Contact Information</h3>
                 <p><i className="fa-solid fa-envelope"></i>SupportTeam@Mindquest.co.uk</p>
                 <p><i className="fa-solid fa-phone"></i>+44 7911036333</p>
-                <p><i className="fa-sharp fa-solid fa-location-dot"></i>31 Kingston Lane<br />Uxbridge<br />Middlesex<br />UB8 3PH</p>
+                <p><i className="fa-sharp fa-solid fa-location-dot"></i>31 Kingston Lane, Uxbridge <br />&emsp;&emsp;Middlesex, UB8 3PH</p>
+            </section>
+
+            {/* Privacy and Security section for footer*/}
+            <section className="footer_section">
+                <h3>Privacy and Security</h3>
+                <ul>
+                    <li><Link className="privacy_button" to="">Privacy Policy</Link></li>
+                    <li><Link className="privacy_button" to="">Terms and Conditions</Link></li>
+                </ul>
             </section>
         </footer>
     );
