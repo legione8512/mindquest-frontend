d672c7dc3c4916a799b2681cb54185817d08633f
Adam Imane <adamdeardon@hotmail.com>
2025-11-21 15:42:43 +0000

    More work done on footer
    - Adjusted some styling to be more efficient.

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,10 +1,8 @@
 export default function Home() {
     return (
         <>
-            <section>
-                <h1>Welcome Home!</h1>
-                <p>Ready to quest?</p>
-            </section>
+            <h1>Welcome Home!</h1>
+            <p>Ready to quest?</p>
         </>
     )
 }
