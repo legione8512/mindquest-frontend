12bc590bda47ec1364b5fa54fa1a21ed0dd53d10
Adam Imane <adamdeardon@hotmail.com>
2025-11-22 12:39:52 +0000

    Profile Page foundation
    - Established the base layout for the profile section of the account page.
    
    - Added an example photo temporarily.
    
    - Added an example Username and Level.

--- front-end/src/pages/Account.jsx
+++ front-end/src/pages/Account.jsx
@@ -1,8 +1,35 @@
+import "./account.css";
+import profile from "../assets/example_profile_picture.jpg";
+
 export default function Profile() {
     return (
         <>
-            <h1>Your Account</h1>
-            <p>Manage your account settings here.</p>
+            <section className="profile_page_body">
+
+                {/* Box containing profile picture, points, and streak */}
+                <section className="profile_box">
+
+                    {/* Profile photo */}
+                    <section>
+                        <img src={profile} alt="Profile Picture" />
+                    </section>
+
+                    {/* Username and level */}
+                    <section className="profile_text_section">
+                        <h3>AdamD6567</h3>
+                        <p>Level 1</p>
+                    </section>
+
+
+
+
+
+                </section>
+
+                <p>Manage your account settings here.</p>
+            </section>
+
+
         </>
     )
 }
