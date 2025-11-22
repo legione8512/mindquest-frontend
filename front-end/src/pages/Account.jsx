c8848e9b4749d99788608547b7cdbd330b3a5267
Adam Imane <adamdeardon@hotmail.com>
2025-11-22 14:48:26 +0000

    Account Page Buttons
    - Added temporary static buttons for Site Settings, Account Settings, and Logout on left side of site settings.
    
    - Added temporary static buttons to cancel or submit form on right side of page.
    
    - TODO: Add functionality to buttons.

--- front-end/src/pages/Account.jsx
+++ front-end/src/pages/Account.jsx
@@ -11,83 +11,102 @@ export default function Account() {
             </section>
 
             {/* Main Body Section */}
-
             <section className="account_page_body">
 
-                {/* Box containing profile picture, points, and streak */}
-                <section className="account_box">
+                {/* Left side content */}
+                <section className="site_settings_left">
 
-                    {/* Top Section of Account box */}
-                    <section className="account_box_top">
+                    {/* Box containing profile picture, points, and streak */}
+                    <section className="account_box">
 
-                        {/* Profile photo */}
-                        <section>
-                            <img src={profile} alt="Profile Picture" />
+                        {/* Top Section of Account box */}
+                        <section className="account_box_top">
+
+                            {/* Profile photo */}
+                            <section>
+                                <img src={profile} alt="Profile Picture" />
+                            </section>
+
+                            {/* Username and level */}
+                            <section className="account_text_section">
+                                <h3>AdamD6567</h3>
+                                <p>Level 1</p>
+                            </section>
                         </section>
 
-                        {/* Username and level */}
-                        <section className="account_text_section">
-                            <h3>AdamD6567</h3>
-                            <p>Level 1</p>
+                        {/* Bottom Section of Account box */}
+                        <section className="account_box_bottom">
+
+                            {/* Points bar */}
+                            <section className="Points_section">
+                                <h3>My Points</h3>
+                                <h3>0</h3>
+                            </section>
+
+                            {/* Streak bar */}
+                            <section className="Streak_section">
+                                <h3>My Streak</h3>
+                                <h3>0d</h3>
+                            </section>
                         </section>
                     </section>
 
-                    {/* Bottom Section of Account box */}
-                    <section className="account_box_bottom">
-
-                        {/* Points bar */}
-                        <section className="Points_section">
-                            <h3>My Points</h3>
-                            <h3>0</h3>
-                        </section>
-
-                        {/* Streak bar */}
-                        <section className="Streak_section">
-                            <h3>My Streak</h3>
-                            <h3>0d</h3>
-                        </section>
+                    {/* Buttons to change page or logout */}
+                    <section className="change_page_buttons">
+                        <button>Site Settings</button>
+                        <button>Account Settings</button>
+                        <button>Logout</button>
                     </section>
                 </section>
 
-                {/* Box containing profile picture, points, and streak */}
-                <section className="site_settings_form_section">
+                {/* Right side content */}
+                <section className="site_settings_right">
+                    {/* Box containing profile picture, points, and streak */}
+                    <section className="site_settings_form_section">
 
-                    {/* Site Settings Heading and Subheading */}
-                    <section className="site_settings_form_top">
-                        <h2>Your Settings</h2>
-                        <p>Site settings</p>
-                    </section>
+                        {/* Site Settings Heading and Subheading */}
+                        <section className="site_settings_form_top">
+                            <h2>Your Settings</h2>
+                            <p>Site settings</p>
+                        </section>
 
-                    {/* Site Settings form */}
-                    <section className="site_settings_form_bottom">
-                        <form className="settings_form">
+                        {/* Site Settings form */}
+                        <section className="site_settings_form_bottom">
+                            <form className="settings_form">
 
-                            {/* Theme dropdown */}
-                            <h4>Theme:</h4>
-                            <select>
-                                <option value="Default">MindQuest Default</option>
-                                <option value="Light">Light</option>
-                                <option value="Dark">Dark</option>
-                            </select>
+                                {/* Theme dropdown */}
+                                <h4>Theme:</h4>
+                                <select>
+                                    <option value="Default">MindQuest Default</option>
+                                    <option value="Light">Light</option>
+                                    <option value="Dark">Dark</option>
+                                </select>
 
-                            {/* Notification Preferences dropdown */}
-                            <h4>Notification Preferences:</h4>
-                            <select>
-                                <option value="Email">Email Only</option>
-                                <option value="SMS">SMS Only</option>
-                                <option value="Email_SMS">Email and SMS</option>
-                            </select>
+                                {/* Notification Preferences dropdown */}
+                                <h4>Notification Preferences:</h4>
+                                <select>
+                                    <option value="Email">Email Only</option>
+                                    <option value="SMS">SMS Only</option>
+                                    <option value="Email_SMS">Email and SMS</option>
+                                </select>
 
-                            {/* Time Zone dropdown */}
-                            <h4>Time Zone:</h4>
-                            <select>
-                                <option value="London">(UTC +00:00) Dublin, Edinburgh, Lisbon, London</option>
-                                <option value="US_East">(UTC-05:00) Eastern Time, US and Canada</option>
-                                <option value="US_Central">(UTC-06:00) Central Time, US and Canada</option>
-                                <option value="US_West">(UTC -08:00) Pacific Time, US and Canada</option>
-                                <option value="Austraila">(UTC+10:00) Brisbane, Austraila</option>
-                            </select>
-                        </form>
+                                {/* Time Zone dropdown */}
+                                <h4>Time Zone:</h4>
+                                <select>
+                                    <option value="London">(UTC +00:00) Dublin, Edinburgh, Lisbon, London</option>
+                                    <option value="US_East">(UTC-05:00) Eastern Time, US and Canada</option>
+                                    <option value="US_Central">(UTC-06:00) Central Time, US and Canada</option>
+                                    <option value="US_West">(UTC -08:00) Pacific Time, US and Canada</option>
+                                    <option value="Austraila">(UTC+10:00) Brisbane, Austraila</option>
+                                </select>
+
+                                {/* Submit and Cancel Button */}
+                                <section className="submit_buttons">
+                                    <button id="cancel">Cancel</button>
+                                    <button id="submit">Submit</button>
+                                </section>
+                            </form>
+                        </section>
                     </section>
                 </section>
             </section>
