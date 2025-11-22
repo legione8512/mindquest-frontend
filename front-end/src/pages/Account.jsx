35a0aa0f82ba59f025dd40937d8f731e50e5875a
Adam Imane <adamdeardon@hotmail.com>
2025-11-22 14:12:01 +0000

    Account Page Site settings Form Dropdowns
    - Added dropdown options for Theme, Notification Preferences, and Time Zone.
    
    - Adjusted Height of Profile box.

--- front-end/src/pages/Account.jsx
+++ front-end/src/pages/Account.jsx
@@ -17,7 +17,9 @@ export default function Account() {
                 {/* Box containing profile picture, points, and streak */}
                 <section className="account_box">
 
+                    {/* Top Section of Account box */}
                     <section className="account_box_top">
+
                         {/* Profile photo */}
                         <section>
                             <img src={profile} alt="Profile Picture" />
@@ -30,29 +32,65 @@ export default function Account() {
                         </section>
                     </section>
 
+                    {/* Bottom Section of Account box */}
                     <section className="account_box_bottom">
+
+                        {/* Points bar */}
                         <section className="Points_section">
                             <h3>My Points</h3>
                             <h3>0</h3>
                         </section>
 
+                        {/* Streak bar */}
                         <section className="Streak_section">
                             <h3>My Streak</h3>
                             <h3>0d</h3>
                         </section>
                     </section>
-
-
-
-
-
-
                 </section>
 
-                <p>Manage your account settings here.</p>
+                {/* Box containing profile picture, points, and streak */}
+                <section className="site_settings_form_section">
+
+                    {/* Site Settings Heading and Subheading */}
+                    <section className="site_settings_form_top">
+                        <h2>Your Settings</h2>
+                        <p>Site settings</p>
+                    </section>
+
+                    {/* Site Settings form */}
+                    <section className="site_settings_form_bottom">
+                        <form className="settings_form">
+
+                            {/* Theme dropdown */}
+                            <h4>Theme:</h4>
+                            <select>
+                                <option value="Default">MindQuest Default</option>
+                                <option value="Light">Light</option>
+                                <option value="Dark">Dark</option>
+                            </select>
+
+                            {/* Notification Preferences dropdown */}
+                            <h4>Notification Preferences:</h4>
+                            <select>
+                                <option value="Email">Email Only</option>
+                                <option value="SMS">SMS Only</option>
+                                <option value="Email_SMS">Email and SMS</option>
+                            </select>
+
+                            {/* Time Zone dropdown */}
+                            <h4>Time Zone:</h4>
+                            <select>
+                                <option value="London">(UTC +00:00) Dublin, Edinburgh, Lisbon, London</option>
+                                <option value="US_East">(UTC-05:00) Eastern Time, US and Canada</option>
+                                <option value="US_Central">(UTC-06:00) Central Time, US and Canada</option>
+                                <option value="US_West">(UTC -08:00) Pacific Time, US and Canada</option>
+                                <option value="Austraila">(UTC+10:00) Brisbane, Austraila</option>
+                            </select>
+                        </form>
+                    </section>
+                </section>
             </section>
-
-
         </>
     )
 }
