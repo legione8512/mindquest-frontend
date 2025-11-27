4cffe05b07ae66a3f1d4bead82fab40b04ec7fc7
Marius-Robert Gheorghe <2436938@brunel.ac.uk>
2025-11-27 19:15:19 +0000

    ok

--- front-end/src/pages/Learning/Learning.jsx
+++ front-end/src/pages/Learning/Learning.jsx
@@ -1,26 +1,33 @@
 import LearningBanner from "../../assets/friends_learning.jpg";
 import "./Learning.css";
 
-
 export default function Learning() {
-    return (
-        <>
-            {/* BANNER */}
-            <section className="learning_banner_wrapper">
-                <img src={LearningBanner} className="learn_banner" />
+  return (
+    <>
+      {/* BANNER */}
+      <section className="learning_banner_wrapper">
+        <img src={LearningBanner} className="learn_banner" />
 
-                <section className="learning_banner_title">
-                    <h1>Learning & Resources</h1>
-                </section>
-            </section>
+        <section className="learning_banner_title">
+          <h1>Learning & Resources</h1>
+        </section>
+      </section>
 
-            {/* Welcome paragraph section */}
-            <section className="learning_introduction_section">
-                <h1>Welcome to the Learning & Resources Page</h1>
-                <p>In this space, you're in charge of your own learning journey. Explore mental health topics at your own pace, whenever you’re ready. Whether you want to dive deep into a specific issue or just browse, the choice is yours.</p>
-                <p>Take your time, absorb the information, and feel free to revisit anytime. This is your space to learn, reflect, and grow, without pressure or timelines.</p>
-            </section>
-
-        </>
-    )
+      {/* Welcome paragraph section */}
+      <section className="learning_introduction_section">
+        <h1>Welcome to the Learning & Resources Page</h1>
+        <p>
+          In this space, you're in charge of your own learning journey. Explore
+          mental health topics at your own pace, whenever you’re ready. Whether
+          you want to dive deep into a specific issue or just browse, the choice
+          is yours.
+        </p>
+        <p>
+          Take your time, absorb the information, and feel free to revisit
+          anytime. This is your space to learn, reflect, and grow, without
+          pressure or timelines.
+        </p>
+      </section>
+    </>
+  );
 }
