c4121f13a8b560a0180d7b36b3372edf619d8ef1
Marius-Robert Gheorghe <2436938@brunel.ac.uk>
2025-11-29 20:33:49 +0000

    changed class to class Name

--- front-end/src/pages/Account/modals/DeleteAccountModal.jsx
+++ front-end/src/pages/Account/modals/DeleteAccountModal.jsx
@@ -4,74 +4,82 @@ import Modal from "react-bootstrap/Modal";
 import "./Modals.css";
 import "./DeleteAccountModal.css";
 
-export default function DeleteAccountModal({ show, onHide, onConfirm, dummyPassword }) {
+export default function DeleteAccountModal({
+  show,
+  onHide,
+  onConfirm,
+  dummyPassword,
+}) {
+  const [enteredPassword, setEnteredPassword] = useState("");
 
-    const [enteredPassword, setEnteredPassword] = useState("");
+  // Handler for confirming the deletion of an account.
+  const confirmDeleteAccount = () => {
+    // Incorrect password entered.
+    if (enteredPassword !== dummyPassword) {
+      alert("Incorrect password. Account not deleted.");
+      return;
+    }
 
-    // Handler for confirming the deletion of an account.
-    const confirmDeleteAccount = () => {
+    // Corret password entered.
+    onConfirm();
+    setEnteredPassword("");
+    return;
+  };
 
-        // Incorrect password entered.
-        if (enteredPassword !== dummyPassword) {
-            alert("Incorrect password. Account not deleted.");
-            return;
-        }
+  // Handler for cancel button
+  const handleCancel = () => {
+    setEnteredPassword("");
+    onHide();
+  };
 
-        // Corret password entered.
-        onConfirm();
-        setEnteredPassword("");
-        return;
+  return (
+    <Modal show={show} onHide={handleCancel} centered size="lg">
+      <Modal.Header>
+        <Modal.Title>Confirm Account Deletion</Modal.Title>
+        <button className="modalCloseButton" onClick={handleCancel}>
+          <i className="fa fa-times"></i>
+        </button>
+      </Modal.Header>
 
-    };
+      <Modal.Body>
+        <section className="warningSection">
+          <h4>
+            Warning: Deleting your account will remove all your stored data!
+          </h4>
+        </section>
 
-    // Handler for cancel button
-    const handleCancel = () => {
-        setEnteredPassword("");
-        onHide();
-    };
+        <p>This includes losing access to:</p>
+        <ul>
+          <li>Your Streaks</li>
+          <li>Your Points</li>
+          <li>Your saved data</li>
+          <li>Our Hubs</li>
+          <li>Our Courses</li>
+        </ul>
+        <p>
+          If you are still sure you want to delete your account, please enter
+          your password to confirm:
+        </p>
 
-    return (
+        {/* Password input */}
+        <input
+          className="deleteAccountField"
+          type="password"
+          placeholder="Enter password"
+          value={enteredPassword}
+          onChange={(e) => setEnteredPassword(e.target.value)}
+        />
+      </Modal.Body>
 
-        <Modal show={show} onHide={handleCancel} centered size="lg">
-
-            <Modal.Header>
-                <Modal.Title>Confirm Account Deletion</Modal.Title>
-                <button className="modalCloseButton" onClick={handleCancel}>
-                    <i class="fa fa-times"></i>
-                </button>
-            </Modal.Header>
-
-            <Modal.Body>
-                <section className="warningSection">
-                    <h4>Warning: Deleting your account will remove all your stored data!</h4>
-                </section>
-
-                <p>This includes losing access to:</p>
-                <ul>
-                    <li>Your Streaks</li>
-                    <li>Your Points</li>
-                    <li>Your saved data</li>
-                    <li>Our Hubs</li>
-                    <li>Our Courses</li>
-                </ul>
-                <p>If you are still sure you want to delete your account, please enter your password to confirm:</p>
-
-                {/* Password input */}
-                <input
-                    className="deleteAccountField"
-                    type="password"
-                    placeholder="Enter password"
-                    value={enteredPassword}
-                    onChange={(e) => setEnteredPassword(e.target.value)}
-                />
-
-            </Modal.Body>
-
-            {/* Submission buttons */}
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleCancel}>Cancel</Button>
-                <Button variant="danger" onClick={confirmDeleteAccount}>Confirm Delete</Button>
-            </Modal.Footer>
-        </Modal >
-    );
+      {/* Submission buttons */}
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleCancel}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={confirmDeleteAccount}>
+          Confirm Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
