183fecc4554bb504219a1187b8d40e4f1da82434
Marius-Robert Gheorghe <2436938@brunel.ac.uk>
2025-11-29 21:04:13 +0000

    managed errors

--- front-end/src/pages/Account/Account.jsx
+++ front-end/src/pages/Account/Account.jsx
@@ -21,279 +21,280 @@ import DeleteAccountModal from "./modals/DeleteAccountModal";
 import LearnMoreModal from "./modals/LearnMoreModal";
 
 export default function Account() {
+  const [profileImage, setProfileImage] = useState(DefaultImage);
+  const [username, setUsername] = useState("AdamD6567"); // Username Placeholder (DEMO PURPOSES).
+  const userLevel = "1"; // Level Placeholder (DEMO PURPOSES).
 
-    const [profileImage, setProfileImage] = useState(DefaultImage);
-    const [username, setUsername] = useState('AdamD6567'); // Username Placeholder (DEMO PURPOSES).
-    const userLevel = "1"; // Level Placeholder (DEMO PURPOSES).
+  // ======================================== SWITCHING FORMS ======================================== //
+  const [currentForm, setCurrentForm] = useState("accountSettings");
 
-    // ======================================== SWITCHING FORMS ======================================== //
-    const [currentForm, setCurrentForm] = useState('accountSettings')
+  // Switching form handler
+  const switchForm = (formName) => {
+    setCurrentForm(formName);
+  };
 
-    // Switching form handler
-    const switchForm = (formName) => {
-        setCurrentForm(formName);
-    }
+  // Log out button
+  const onLogOut = () => {
+    alert("Logging user out...");
+  };
 
-    // Log out button
-    const onLogOut = () => {
-        alert("Logging user out...");
-    }
+  // ========================================  ACCOUNT SETTINGS FORM ======================================== //
 
+  // Dummy values (DEMO PURPOSES)
+  const dummyUsernames = ["Adam", "Marius", "Harshil", "Alex", "Aayisha"];
+  const dummyEmails = [
+    "Adam@brunel.ac.uk",
+    "Marius@brunel.ac.uk",
+    "Harshil@brunel.ac.uk",
+    "Alex@brunel.ac.uk",
+    "Aayisha@brunel.ac.uk",
+  ];
+  const dummyPhones = [
+    "07115201776",
+    "07289994594",
+    "07530773350",
+    "07523697780",
+    "07457319635",
+  ];
+  const [dummyPassword, setDummyPassword] = useState("Password123!");
 
-    // ========================================  ACCOUNT SETTINGS FORM ======================================== //
+  // Default state of account form (DEMO PURPOSES)
+  const [accountSettings, setAccountSettings] = useState({
+    username: "AdamD6567",
+    email: "2425290@brunel.ac.uk",
+    phone_no: "07827753053",
+    password: "",
+    verify_password: "",
+  });
 
-    // Dummy values (DEMO PURPOSES)
-    const dummyUsernames = ["Adam", "Marius", "Harshil", "Alex", "Aayisha"];
-    const dummyEmails = ["Adam@brunel.ac.uk", "Marius@brunel.ac.uk", "Harshil@brunel.ac.uk", "Alex@brunel.ac.uk", "Aayisha@brunel.ac.uk"];
-    const dummyPhones = ["07115201776", "07289994594", "07530773350", "07523697780", "07457319635"];
-    const [dummyPassword, setDummyPassword] = useState("Password123!");
+  // Track previously submitted account values (DEMO PURPOSES)
+  const [savedAccountSettings, setSavedAccountSettings] = useState({
+    username: "AdamD6567",
+    email: "2425290@brunel.ac.uk",
+    phone_no: "07827753053",
+  });
 
-    // Default state of account form (DEMO PURPOSES)
-    const [accountSettings, setAccountSettings] = useState({
-        username: "AdamD6567",
-        email: "2425290@brunel.ac.uk",
-        phone_no: "07827753053",
-        password: "",
-        verify_password: ""
-    })
+  // Change handler when user is inputting values
+  const setAccountSetting = (field) => {
+    return ({ target: { value } }) => {
+      setAccountSettings((prevValue) => ({ ...prevValue, [field]: value }));
+    };
+  };
 
-    // Track previously submitted account values (DEMO PURPOSES)
-    const [savedAccountSettings, setSavedAccountSettings] = useState({
-        username: "AdamD6567",
-        email: "2425290@brunel.ac.uk",
-        phone_no: "07827753053"
+  // Submit button
+  const onSubmitAccountSettings = (e) => {
+    e.preventDefault();
+
+    // Validate all fields in the form on submit
+    const error = validateAccountSettings(accountSettings, {
+      dummyUsernames,
+      dummyEmails,
+      dummyPhones,
+      dummyPassword,
     });
 
-    // Change handler when user is inputting values
-    const setAccountSetting = field => {
-        return ({ target: { value } }) => {
-            setAccountSettings(prevValue => ({ ...prevValue, [field]: value }));
-        };
-    };
-
-    // Submit button
-    const onSubmitAccountSettings = (e) => {
-        e.preventDefault();
-
-        // Validate all fields in the form on submit
-        const error = validateAccountSettings(
-            accountSettings,
-            {
-                dummyUsernames,
-                dummyEmails,
-                dummyPhones,
-                dummyPassword
-            }
-        );
-
-        // INVALID VALUES: Display an error
-        if (error) {
-            alert(error);
-            return;
-        }
-
-        // VALID VALUES: Update user data
-        setUsername(accountSettings.username);
-
-        // Only update password if the user has chosen to
-        if (accountSettings.password.trim() != "") {
-            setDummyPassword(accountSettings.password);
-        }
-
-        // Save submitted account settings for tracking (DEMO PURPOSES)
-        setSavedAccountSettings({
-            username: accountSettings.username,
-            email: accountSettings.email,
-            phone_no: accountSettings.phone_no
-        });
-
-        // Refresh form with new values.
-        setAccountSettings({
-            username: accountSettings.username,
-            email: accountSettings.email,
-            phone_no: accountSettings.phone_no,
-            password: "",
-            verify_password: ""
-        });
-
-        alert("Your account settings have been updated!");
-
-    };
-
-    // Cancel button
-    const onCancelAccountSettings = (e) => {
-        e.preventDefault();
-
-        // Revert to previously submitted settings (DEMO PURPOSES)
-        setAccountSettings({
-            ...savedAccountSettings,
-            password: "",
-            verify_password: ""
-        });
-
-        alert("Changes cancelled, no settings changed.")
-
+    // INVALID VALUES: Display an error
+    if (error) {
+      alert(error);
+      return;
     }
 
+    // VALID VALUES: Update user data
+    setUsername(accountSettings.username);
 
-    // ======================================== DELETE ACCOUNT ======================================== //
-
-    // Modal box
-    const [showDeleteModal, setShowDeleteModal] = useState(false);
-
-    // Delete Account button.
-    const deleteAccount = () => {
-
-        // Set all values as empty (DEMO PURPOSES)
-        setAccountSettings({
-            username: "",
-            email: "",
-            phone_no: "",
-            password: "",
-            verify_password: ""
-        });
-        setUserSiteSettings({
-            theme: "",
-            notification: "",
-            timeZone: ""
-        });
-        setUsername("");
-        setProfileImage(EmptyImage);
-
-        alert("You have deleted your account. Goodbye!");
-        setShowDeleteModal(false);
-
+    // Only update password if the user has chosen to
+    if (accountSettings.password.trim() != "") {
+      setDummyPassword(accountSettings.password);
     }
 
-
-    // ========================================  SITE SETTINGS FORM ======================================== //
-
-    // Default state of site form (DEMO PURPOSES)
-    const [userSiteSettings, setUserSiteSettings] = useState({
-        theme: 'MindQuest Default',
-        notification: 'None',
-        timeZone: 'London Time'
+    // Save submitted account settings for tracking (DEMO PURPOSES)
+    setSavedAccountSettings({
+      username: accountSettings.username,
+      email: accountSettings.email,
+      phone_no: accountSettings.phone_no,
     });
 
-    // Track previously submitted account values (DEMO PURPOSES)
-    const [savedSiteSettings, setSavedSiteSettings] = useState({
-        userSiteSettings
+    // Refresh form with new values.
+    setAccountSettings({
+      username: accountSettings.username,
+      email: accountSettings.email,
+      phone_no: accountSettings.phone_no,
+      password: "",
+      verify_password: "",
     });
 
-    // Change handler when user is inputting values
-    const setSiteSetting = field => {
-        return ({ target: { value } }) => {
-            setUserSiteSettings(prevValue => ({ ...prevValue, [field]: value }));
-        }
+    alert("Your account settings have been updated!");
+  };
+
+  // Cancel button
+  const onCancelAccountSettings = (e) => {
+    e.preventDefault();
+
+    // Revert to previously submitted settings (DEMO PURPOSES)
+    setAccountSettings({
+      ...savedAccountSettings,
+      password: "",
+      verify_password: "",
+    });
+
+    alert("Changes cancelled, no settings changed.");
+  };
+
+  // ======================================== DELETE ACCOUNT ======================================== //
+
+  // Modal box
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  // Delete Account button.
+  const deleteAccount = () => {
+    // Set all values as empty (DEMO PURPOSES)
+    setAccountSettings({
+      username: "",
+      email: "",
+      phone_no: "",
+      password: "",
+      verify_password: "",
+    });
+    setUserSiteSettings({
+      theme: "",
+      notification: "",
+      timeZone: "",
+    });
+    setUsername("");
+    setProfileImage(EmptyImage);
+
+    alert("You have deleted your account. Goodbye!");
+    setShowDeleteModal(false);
+  };
+
+  // ========================================  SITE SETTINGS FORM ======================================== //
+
+  // Default state of site form (DEMO PURPOSES)
+  const [userSiteSettings, setUserSiteSettings] = useState({
+    theme: "MindQuest Default",
+    notification: "None",
+    timeZone: "London Time",
+  });
+
+  // Track previously submitted account values (DEMO PURPOSES)
+  const [savedSiteSettings, setSavedSiteSettings] = useState({
+    userSiteSettings,
+  });
+
+  // Change handler when user is inputting values
+  const setSiteSetting = (field) => {
+    return ({ target: { value } }) => {
+      setUserSiteSettings((prevValue) => ({ ...prevValue, [field]: value }));
     };
+  };
 
-    // Submit button
-    const onSubmitSiteSettings = (e) => {
-        e.preventDefault();
+  // Submit button
+  const onSubmitSiteSettings = (e) => {
+    e.preventDefault();
 
-        // If user does not have an assoicated phone number
-        if (userSiteSettings.notification == "SMS Only" && savedAccountSettings.phone_no == "" ||
-            userSiteSettings.notification === "Email and SMS" && savedAccountSettings.phone_no == "") {
+    // If user does not have an assoicated phone number
+    if (
+      (userSiteSettings.notification == "SMS Only" &&
+        savedAccountSettings.phone_no == "") ||
+      (userSiteSettings.notification === "Email and SMS" &&
+        savedAccountSettings.phone_no == "")
+    ) {
+      alert(
+        "You do not have an associated phone number. Enter a phone number, or select a different option."
+      );
 
-            alert("You do not have an associated phone number. Enter a phone number, or select a different option.")
-
-            return;
-
-        }
-
-        // VALID VALUES: Update user data
-        const { theme, notification, timeZone } = userSiteSettings;
-        setSavedSiteSettings(userSiteSettings);
-
-        alert(`Your site settings were successfully updated!\n\nTheme: ${theme}\nNotification Preferences: ${notification}\nTimeZone: ${timeZone}`);
-
-    };
-
-    // Cancel button.
-    const onCancelSiteSettings = (e) => {
-        e.preventDefault();
-
-        // Revert to previously submitted settings (DEMO PURPOSES)
-        setUserSiteSettings(savedSiteSettings);
-        alert("Changes cancelled, no settings changed.")
-
+      return;
     }
 
-    // Learn more modal //
-    const [showLearnMoreModal, setShowLearnMoreModal] = useState(false);
+    // VALID VALUES: Update user data
+    const { theme, notification, timeZone } = userSiteSettings;
+    setSavedSiteSettings(userSiteSettings);
 
+    alert(
+      `Your site settings were successfully updated!\n\nTheme: ${theme}\nNotification Preferences: ${notification}\nTimeZone: ${timeZone}`
+    );
+  };
 
-    // ========================================  ACCOUNT PAGE RENDER ======================================== //
-    return (
-        <>
-            {/* PAGE BANNER */}
-            <section className="account_page_banner">
-                <h1> {currentForm === 'accountSettings' ? 'Account Settings' : 'Site Settings'} </h1>
-            </section>
+  // Cancel button.
+  const onCancelSiteSettings = (e) => {
+    e.preventDefault();
 
-            {/* PAGE CONTENT */}
-            <section className="account_page">
+    // Revert to previously submitted settings (DEMO PURPOSES)
+    setUserSiteSettings(savedSiteSettings);
+    alert("Changes cancelled, no settings changed.");
+  };
 
-                {/* LEFT SIDE SECTION: Mini Profile + Navigation/Logout buttons */}
-                <section className="account_page_left">
+  // Learn more modal //
+  const [showLearnMoreModal, setShowLearnMoreModal] = useState(false);
 
-                    <ProfileBox
-                        profileImage={profileImage}
-                        setProfileImage={setProfileImage}
-                        username={username}
-                        userLevel={userLevel}
-                    />
+  // ========================================  ACCOUNT PAGE RENDER ======================================== //
+  return (
+    <>
+      {/* PAGE BANNER */}
+      <section className="account_page_banner">
+        <h1>
+          {" "}
+          {currentForm === "accountSettings"
+            ? "Account Settings"
+            : "Site Settings"}{" "}
+        </h1>
+      </section>
 
-                    <FormNavigation
-                        currentForm={currentForm}
-                        switchForm={switchForm}
-                        onLogOut={onLogOut}
-                    />
+      {/* PAGE CONTENT */}
+      <section className="account_page">
+        {/* LEFT SIDE SECTION: Mini Profile + Navigation/Logout buttons */}
+        <section className="account_page_left">
+          <ProfileBox
+            profileImage={profileImage}
+            setProfileImage={setProfileImage}
+            username={username}
+            userLevel={userLevel}
+          />
 
-                </section>
+          <FormNavigation
+            currentForm={currentForm}
+            switchForm={switchForm}
+            onLogOut={onLogOut}
+          />
+        </section>
 
-                {/* RIGHT SIDE SECTION: Forms */}
-                <section className="account_page_right">
-
-                    {currentForm === 'accountSettings' &&
-                        <AccountSettingsForm
-                            accountSettings={accountSettings}
-                            setAccountSetting={setAccountSetting}
-                            onSubmit={onSubmitAccountSettings}
-                            onCancel={onCancelAccountSettings}
-                            onDelete={() => setShowDeleteModal(true)}
-
-                        />
-                    }
-
-                    {currentForm === 'siteSettings' &&
-                        <SiteSettingsForm
-                            userSiteSettings={userSiteSettings}
-                            setSiteSetting={setSiteSetting}
-                            onSubmit={onSubmitSiteSettings}
-                            onCancel={onCancelSiteSettings}
-                            onLearnMore={() => setShowLearnMoreModal(true)}
-                        />
-                    }
-
-                </section>
-
-            </section>
-
-            {/* DELETE ACCOUNT MODAL BOX */}
-            <DeleteAccountModal
-                show={showDeleteModal}
-                onHide={() => setShowDeleteModal(false)}
-                onConfirm={deleteAccount}
-                dummyPassword={dummyPassword}
+        {/* RIGHT SIDE SECTION: Forms */}
+        <section className="account_page_right">
+          {currentForm === "accountSettings" && (
+            <AccountSettingsForm
+              accountSettings={accountSettings}
+              setAccountSetting={setAccountSetting}
+              onSubmit={onSubmitAccountSettings}
+              onCancel={onCancelAccountSettings}
+              onDelete={() => setShowDeleteModal(true)}
             />
+          )}
 
-            {/* LEARN MORE MODAL BOX */}
-            <LearnMoreModal
-                show={showLearnMoreModal}
-                onHide={() => setShowLearnMoreModal(false)}
+          {currentForm === "siteSettings" && (
+            <SiteSettingsForm
+              userSiteSettings={userSiteSettings}
+              setSiteSetting={setSiteSetting}
+              onSubmit={onSubmitSiteSettings}
+              onCancel={onCancelSiteSettings}
+              onLearnMore={() => setShowLearnMoreModal(true)}
             />
+          )}
+        </section>
+      </section>
 
-        </>
-    )
+      {/* DELETE ACCOUNT MODAL BOX */}
+      <DeleteAccountModal
+        show={showDeleteModal}
+        onHide={() => setShowDeleteModal(false)}
+        onConfirm={deleteAccount}
+        dummyPassword={dummyPassword}
+      />
+
+      {/* LEARN MORE MODAL BOX */}
+      <LearnMoreModal
+        show={showLearnMoreModal}
+        onHide={() => setShowLearnMoreModal(false)}
+      />
+    </>
+  );
 }
