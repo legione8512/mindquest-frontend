2e5625f5a59f8f87a6beb9efab9fe4ecb79b9c69
Adam Imane <adamdeardon@hotmail.com>
2025-11-29 20:35:31 +0000

    Learning Page: Lesson page navigation
    - Added navigation functionality to the page, so now the user can press the next and back buttons to navigate between the pages in the lesson.
    
    - TODO: Styling for the page.

--- front-end/src/pages/Learning/LessonPage.jsx
+++ front-end/src/pages/Learning/LessonPage.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Learning.css";
 
-export default function AnxietyLesson() {
+export default function LessonPage() {
 
     // Navigation between lesson pages.
     const navigate = useNavigate();
+    const [currentPage, setCurrentPage] = useState(1);
+    const nextPage = () => setCurrentPage((page) => page + 1);
+    const prevPage = () => setCurrentPage((page) => page - 1);
 
     return (
 
@@ -16,74 +20,75 @@ export default function AnxietyLesson() {
             </section>
 
             {/* Page 1 */}
-            <section className="lesson_section">
-                <p>
-                    Anxiety is often described as a feeling of fear or unease -
-                    and it's something everyone experiences at times. Feeling anxious is
-                    a perfectly natural reaction to some situations.
-                </p>
+            {currentPage === 1 && (
+                <section className="lesson_section">
+                    <p>
+                        Anxiety is often described as a feeling of fear or unease -
+                        and it's something everyone experiences at times. Feeling anxious is
+                        a perfectly natural reaction to some situations.
+                    </p>
 
-                <p>
-                    Anxiety can become a problem if we start worrying a lot about small stuff or
-                    relatively harmless situations.It's usually when our anxiety feels really intense
-                    or overwhelming that it starts to interfere with our daily life or affect our relationships.
-                </p>
-            </section>
+                    <p>
+                        Anxiety can become a problem if we start worrying a lot about small stuff or
+                        relatively harmless situations.It's usually when our anxiety feels really intense
+                        or overwhelming that it starts to interfere with our daily life or affect our relationships.
+                    </p>
+                </section>
+            )}
 
             {/* Page 2 */}
-            <section className="lesson_section">
-                <p>
-                    Anxiety can affect our mind, body and behaviour, for instance we might feel tearful,
-                    get stress headaches, or start avoiding things or people that trigger anxiety.
-                </p>
+            {currentPage === 2 && (
+                <section className="lesson_section">
+                    <p>
+                        Anxiety can affect our mind, body and behaviour, for instance we might feel tearful,
+                        get stress headaches, or start avoiding things or people that trigger anxiety.
+                    </p>
 
-                <p>
-                    Common symptoms of anxiety include:
-                </p>
+                    <p>
+                        Common symptoms of anxiety include:
+                    </p>
 
-                <ul>
-                    <li>Feeling tired, restless or irritable</li>
-                    <li>Feeling shaky or trembly, dizzy or sweating more</li>
-                    <li>Being unable to concentrate or make decisions</li>
-                    <li>Trouble sleeping</li>
-                    <li>Worrying about the past or future, or thinking something bad will happen</li>
-                </ul>
-            </section>
+                    <ul>
+                        <li>Feeling tired, restless or irritable</li>
+                        <li>Feeling shaky or trembly, dizzy or sweating more</li>
+                        <li>Being unable to concentrate or make decisions</li>
+                        <li>Trouble sleeping</li>
+                        <li>Worrying about the past or future, or thinking something bad will happen</li>
+                    </ul>
+                </section>
+            )}
 
             {/* Page 3 */}
-            <section className="lesson_section">
-                <p>
-                    Anxiety is caused by many different situations and life experiences.
-                </p>
+            {currentPage === 3 && (
+                <section className="lesson_section">
+                    <p>
+                        Anxiety is caused by many different situations and life experiences.
+                    </p>
 
-                <p>
-                    How anxiety affects us is very personal to us, and if you asked 100 people what it means to them,
-                    you'd probably get 100 different answers.
-                </p>
+                    <p>
+                        How anxiety affects us is very personal to us, and if you asked 100 people what it means to them,
+                        you'd probably get 100 different answers.
+                    </p>
 
-                <p>
-                    Sometimes there are no obvious triggers for it and it's difficult to know what causes anxiety,
-                    which can be upsetting or stressful in itself.
-                </p>
+                    <p>
+                        Sometimes there are no obvious triggers for it and it's difficult to know what causes anxiety,
+                        which can be upsetting or stressful in itself.
+                    </p>
 
-                <p>
-                    Everyone's anxiety levels are different. Some people find more situations stressful and experience
-                    more challenges in life than others, and they get more anxious as a result.
-                </p>
+                    <p>
+                        Everyone's anxiety levels are different. Some people find more situations stressful and experience
+                        more challenges in life than others, and they get more anxious as a result.
+                    </p>
 
-            </section>
+                </section>
+            )}
 
             {/* Navigation Buttons */}
             <section className="lesson_nav_buttons">
-                <button onClick={() => navigate(-1)}>Back</button>
-                <button onClick={() => navigate("/learning")}>Learning Home</button>
+                {currentPage > 1 && <button onClick={prevPage}>Previous page</button>}
+                {currentPage < 3 && <button onClick={nextPage}>Next page</button>}
+                <button onClick={() => navigate("/learning")}>Back to the Learning page</button>
             </section>
-
         </section>
-
-
-
     )
-
-
 }
