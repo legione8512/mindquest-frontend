ddbce4e1a03cd530631662029a06e2c6124c38e9
Marius-Robert Gheorghe <2436938@brunel.ac.uk>
2025-11-30 20:59:14 +0000

    ammend

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 /* Import for navigation bar and sidebar */
 import Navigation from "./components/navigation/navigation.jsx";
@@ -7,13 +7,14 @@ import Sidebar from "./components/sidebar/sidebar.jsx";
 import Footer from "./components/footer/footer.jsx";
 
 /* Page imports */
-import Home from "./pages/Home.jsx";
+import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard.jsx";
 import Learning from "./pages/Learning/Learning.jsx";
-import MoodCompass from "./pages/MoodCompass.jsx";
+import MoodCompass from "./pages/MoodCompass/MoodCompass.jsx";
 import Account from "./pages/Account/Account.jsx";
-import Register from "./pages/Register.jsx";
+import Register from "./pages/Register/Register.jsx";
 import QuestsHubs from "./pages/QuestsHubs/QuestsHubs.jsx";
+import QuickCalm from "./pages/QuickCalm.jsx";
 
 /* Components Import */
 import "./components/sidebar/sidebar.css";
@@ -28,8 +29,8 @@ function App() {
   return (
     // Navigation between pages
     <Router>
-      <Navigation toggleSidebar={toggleSidebar}/>
-      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+      <Navigation toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -38,10 +39,11 @@ function App() {
         <Route path="/mood_compass" element={<MoodCompass />} />
         <Route path="/register" element={<Register />} />
         <Route path="/hubs" element={<QuestsHubs />} />
+        <Route path="/quickcalm" element={<QuickCalm />} />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
