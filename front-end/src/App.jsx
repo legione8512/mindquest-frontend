e981d0fc3856c389dc90d6107b0b2bcad6fa14b7
Marius-Robert Gheorghe <2436938@brunel.ac.uk>
2025-12-01 12:30:43 +0000

    errors 2

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -16,7 +16,6 @@ import QuestsHubs from "./pages/QuestsHubs/QuestsHubs.jsx";
 import QuickCalm from "./pages/QuickCalm.jsx";
 import MoodCompass from "./pages/MoodCompass/MoodCompass.jsx";
 import Register from "./pages/Register/Register.jsx";
-import QuickCalm from "./pages/QuickCalm.jsx";
 
 /* Components Import */
 import "./components/sidebar/sidebar.css";
