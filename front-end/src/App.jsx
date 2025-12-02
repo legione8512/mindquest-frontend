b94aedde02badf888e02cfce4a1f5af2ae6b6853
Aayishazaa <aayishaahamed@yahoo.com>
2025-12-02 22:49:57 +0000

    footer

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/footer/Footer.jsx';
 
 /* Import for navigation bar and sidebar */
 import Navigation from "./components/navigation/navigation.jsx";
